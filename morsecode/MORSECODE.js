93b29e059b4d6f8f546836e7359da8dfbdc1ce72
BeastRush1231 <M0761005@g2.usc.edu.tw>
2019-11-18 20:13:54 +0800

    1118 MSP-morsecode-6

---
+++ morsecode/MORSECODE.js
@@ -0,0 +1,10 @@
+// Morsecode array
+var MorsecodeArray = "A;.-|B;-...|C;-.-.|D;-..|E;.|F;..-.|G;--.|H;....|I;..|J;.---|K;-.-|L;.-..|M;--|N;-.|O;---|P;.--.|Q;--.-|R;.-.|S;...|T;-|U;..-|V;...-|W;.--|X;-..-|Y;-.--|Z;--..|/;-..-.|1;.----|2;..---|3;...--|4;....-|5;.....|6;-....|7;--...|8;---..|9;----.|0;-----"
+
+let MorsecodeList = MorsecodeArray.split("|");
+var translateUL = document.querySelector(".translist");
+
+for(let i=0; i < MorsecodeList.length; i++) {
+  translateUL.innerHTML(`<li>${MorsecodeList[i][0]}</li>`);
+  MorsecodeList[i] = MorsecodeList[i].split(";");
+}
